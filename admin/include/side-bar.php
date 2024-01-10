a4e0d326fe97b3ff76fb63877bc9224150fe368d
Yusuf <ysaifullah28kbg@gmail.com>
2024-01-10 11:51:00 +0100

    formatted the admin sidebar

--- admin/include/side-bar.php
+++ admin/include/side-bar.php
@@ -1,47 +1,47 @@
 <!-- Sidebar -->
-    <ul class="sidebar navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="dashboard.php">
-          <i class="fas fa-fw fa-tachometer-alt"></i>
-          <span>Dashboard</span>
-        </a>
-      </li>
-      
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: white;">
-          <i class="fa fa-list-alt"></i>
-          <span>Manage Category</span>
-        </a>
-        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
-          <a class="dropdown-item" href="add-incomecategory.php">
-            <i class="fa fa-list-alt"></i>
-            Add Income Category</a>
-             <!-- <a class="dropdown-item" href="view-category.php">View Category</a> -->
-          <a class="dropdown-item" href="add-expensecategory.php">
-          <i class="fa fa-list-alt"></i>
-          Add Expense Category</a>
+<ul class="sidebar navbar-nav">
+  <li class="nav-item active">
+    <a class="nav-link" href="dashboard.php">
+      <i class="fas fa-fw fa-tachometer-alt"></i>
+      <span>Dashboard</span>
+    </a>
+  </li>
 
-      </div>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: white;">
-          <i class="fa fa-list-alt"></i>
-          <span>Manage Reports</span>
-        </a>
-        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
-          <a class="dropdown-item" href="view-incomecategory.php">View Income Category</a>
-          <a class="dropdown-item" href="view-expensecategory.php">View Expense Category</a>
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: white;">
+      <i class="fa fa-list-alt"></i>
+      <span>Manage Category</span>
+    </a>
+    <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+      <a class="dropdown-item" href="add-incomecategory.php">
+        <i class="fa fa-list-alt"></i>
+        Add Income Category</a>
+      <!-- <a class="dropdown-item" href="view-category.php">View Category</a> -->
+      <a class="dropdown-item" href="add-expensecategory.php">
+        <i class="fa fa-list-alt"></i>
+        Add Expense Category</a>
 
-      </div>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: white;">
-          <i class="fa fa-user"></i>
-          <span>Manage Users</span>
-        </a>
-        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
-          <a class="dropdown-item" href="add-users.php">Add Users</a>
-          <a class="dropdown-item" href="view-users.php">View Users</a>
-      </div>
-      </li>
-     </ul>
+    </div>
+  </li>
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: white;">
+      <i class="fa fa-list-alt"></i>
+      <span>Manage Reports</span>
+    </a>
+    <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+      <a class="dropdown-item" href="view-incomecategory.php">View Income Category</a>
+      <a class="dropdown-item" href="view-expensecategory.php">View Expense Category</a>
+
+    </div>
+  </li>
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: white;">
+      <i class="fa fa-user"></i>
+      <span>Manage Users</span>
+    </a>
+    <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+      <a class="dropdown-item" href="add-users.php">Add Users</a>
+      <a class="dropdown-item" href="view-users.php">View Users</a>
+    </div>
+  </li>
+</ul>
